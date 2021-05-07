faedc8c1c7dd701d7d0289c4dff678653c37126b
Abdulrahman Abouzaid <aabouzaid96@gmail.com>
2021-05-07 02:32:35 +0200

    Create Day 6   Convert Sorted List to Binary Search Tree.java

---
+++ May 2021 Challenge/Day 6   Convert Sorted List to Binary Search Tree.java
@@ -0,0 +1,3 @@
+// using Slow and fast pointers algorithm -> this used to get the middle value of linkedlist  
+// if it is an array, we will get middle using indices directly
+// complextity O(nlogn)
