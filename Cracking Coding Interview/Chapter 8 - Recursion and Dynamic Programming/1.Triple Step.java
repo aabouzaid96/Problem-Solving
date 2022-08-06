11f61334377fd53ebe3381da2cbd9fb9a26905f9
Abdulrahman Abouzaid <aabouzaid96@gmail.com>
2022-08-06 18:48:23 +0200

    Update 1.Triple Step.java

--- Cracking Coding Interview/Chapter 8 - Recursion and Dynamic Programming/1.Triple Step.java
+++ Cracking Coding Interview/Chapter 8 - Recursion and Dynamic Programming/1.Triple Step.java
@@ -1,3 +1,7 @@
+Problem 8.1: Triple Step: A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3
+steps at a time. Implement a method to count how many possible ways the child can run up the
+stairs.
+
 Same: 1137. N-th Tribonacci Number
 https://leetcode.com/problems/n-th-tribonacci-number/
 
