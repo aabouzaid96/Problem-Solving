da24ed52cb0469604ac3d6b6e1d4efbd4feb0494
Abdulrahman Abouzaid <aabouzaid96@gmail.com>
2022-08-19 00:35:21 +0200

    Update 6. Conversion.java

--- Cracking Coding Interview/Chapter 5 - Bit Manipulation/6. Conversion.java
+++ Cracking Coding Interview/Chapter 5 - Bit Manipulation/6. Conversion.java
@@ -1,5 +1,5 @@
 /*
-Conversion: Write a function to determine the number of bits you would need to flip to convert
+5.6 Conversion: Write a function to determine the number of bits you would need to flip to convert
 integer A to integer B.
 EXAMPLE
 Input: 29 (or: 11101), 15 (or: 01111)
