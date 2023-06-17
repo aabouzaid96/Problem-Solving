b47f29e8888ec1cc98efea0404559769700d8c20
Abdulrahman Abouzaid <aabouzaid96@gmail.com>
2023-06-17 16:20:38 +0300

    Update 283. Move Zeroes.java

--- LeetCode/Leetcode Community session/283. Move Zeroes.java
+++ LeetCode/Leetcode Community session/283. Move Zeroes.java
@@ -1,43 +1,50 @@
-// optimized- clean code solution O(N) time, O(1) space
+// Siolution 1: Optimized - clean code solution O(N) time, O(1) space
+    class Solution {
+        public int[] moveZeroes(int[] nums) {
+            int k = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    swap(nums, k++, i);
+                }
+            }
+            return nums;
+        }
+
+        private void swap(int[] nums, int x, int y) {
+            int temp = nums[x];
+            nums[x] = nums[y];
+            nums[y] = temp;
+        }
+    }
+
+
+// Solution 2 two pointers  solution O(N) time, O(1) space
 class Solution {
-	public int[] moveZeroes(int[] nums) {
-		int k = 0;
-		for (int i = 0; i < nums.length; i++) {
-				if (nums[i] != 0) {
-						swap(nums, k++, i);
-				}
-		}
-		return nums;
-	}
-	private void swap(int[] nums, int x, int y){
-		int temp = nums[x];
-		nums[x] = nums[y];
-		nums[y] = temp;
-	}
-}
-// two pointers  solution O(N) time, O(1) space
-class Solution {
-  public int[] moveZeroes(int[] nums) {
-	  int l=0, r=1;
-		while(l<nums.length && r<nums.length){
-			int leftValue = nums[l];
-			int rightValue = nums[r];
-			if(leftValue != rightValue){
-				if(leftValue == 0)
-					swap(nums, l, r);
-				l++;
-				r++;
-			}else{
-				if(leftValue == 0) r++;
-				else {l+=2; r+=2; }
-			}
-		}
-		
-    return nums;
-  }
-	private void swap(int[] nums, int x, int y){
-		int temp = nums[x];
-		nums[x] = nums[y];
-		nums[y] = temp;
-	}
-}
+        public int[] moveZeroes(int[] nums) {
+            int l = 0, r = 1;
+            while (l < nums.length && r < nums.length) {
+                int leftValue = nums[l];
+                int rightValue = nums[r];
+                if (leftValue != rightValue) {
+                    if (leftValue == 0)
+                        swap(nums, l, r);
+                    l++;
+                    r++;
+                } else {
+                    if (leftValue == 0) r++;
+                    else {
+                        l += 2;
+                        r += 2;
+                    }
+                }
+            }
+
+            return nums;
+        }
+
+        private void swap(int[] nums, int x, int y) {
+            int temp = nums[x];
+            nums[x] = nums[y];
+            nums[y] = temp;
+        }
+    }
